d09abc9009286e8739d41de8bec216a3a68837e3
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-11-08 19:29:59 -0500

    Bump beta version

--- Controllers/RootOptionsController.m
+++ Controllers/RootOptionsController.m
@@ -279,7 +279,7 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
     if (section == 3) {
-        return @"Version: 4.0.7";
+        return @"Version: 4.0.8 (Beta)";
     }
     return nil;
 }
