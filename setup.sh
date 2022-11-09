1b1ae5d2188287e170969bfc1d377ffea21e4add
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-11-09 18:21:43 -0500

    Revert "Change to FFmpegKit part 2"
    This reverts commit 27d71d80ef8c5ac278119bb729d835c1e8951cd8.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 set -e
-rm -rf layout/Library/Frameworks ffmpeg-kit-full-5.1.LTS-ios-framework.zip
+rm -rf Frameworks ffmpeg-kit-full-5.1.LTS-ios-framework.zip
 curl -LO https://github.com/arthenica/ffmpeg-kit/releases/download/v5.1.LTS/ffmpeg-kit-full-5.1.LTS-ios-framework.zip
-unzip ffmpeg-kit-full-5.1.LTS-ios-framework.zip -d layout/Library/Frameworks
+unzip ffmpeg-kit-full-5.1.LTS-ios-framework.zip -d Frameworks
 rm -rf ffmpeg-kit-full-5.1.LTS-ios-framework.zip
 echo Done
