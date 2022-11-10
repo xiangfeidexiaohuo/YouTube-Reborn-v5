61209214add8f80a23493d3e400be885c11a248d
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-11-10 13:26:45 -0500

    Update changelog url

--- Controllers/ChangelogsController.m
+++ Controllers/ChangelogsController.m
@@ -9,7 +9,7 @@
 	[super loadView];
     
     self.rebornChangelogsWebView = [[WKWebView alloc] initWithFrame:self.view.frame];  
-    [self.rebornChangelogsWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://lillieh001.github.io/repo/changelogs/youtuberebornv4.html"]]];
+    [self.rebornChangelogsWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://lillieh001.github.io/changelogs/youtuberebornv4.html"]]];
     self.rebornChangelogsWebView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height);
     [self.view addSubview:self.rebornChangelogsWebView];
 }
