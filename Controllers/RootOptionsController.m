5c0f654463fc08a72ed144853581bb3586c9e41f
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-12-07 20:33:39 -0500

    Remove patreon info from settings

--- Controllers/RootOptionsController.m
+++ Controllers/RootOptionsController.m
@@ -47,7 +47,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (section == 0) {
-        return 3;
+        return 2;
     }
     if (section == 1) {
         if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"filza://"]]) {
@@ -86,12 +86,9 @@
         if (indexPath.section == 0) {
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
             if (indexPath.row == 0) {
-                cell.textLabel.text = @"Patreon";
-            }
-            if (indexPath.row == 1) {
                 cell.textLabel.text = @"Discord";
             }
-            if (indexPath.row == 2) {
+            if (indexPath.row == 1) {
                 cell.textLabel.text = @"Reddit";
             }
         }
@@ -151,12 +148,9 @@
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     if (indexPath.section == 0) {
         if (indexPath.row == 0) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.patreon.com/lillieweeb"] options:@{} completionHandler:nil];
-        }
-        if (indexPath.row == 1) {
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://lillieh001.github.io/discord/youtuberebornv4.html"] options:@{} completionHandler:nil];
         }
-        if (indexPath.row == 2) {
+        if (indexPath.row == 1) {
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.reddit.com/r/YouTubeReborn/"] options:@{} completionHandler:nil];
         }
     }
