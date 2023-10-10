1934becce8bd34cf7f433d2a37f5241e9e8956bb
arichorn <78001398+arichorn@users.noreply.github.com>
2023-10-10 16:59:03 -0500

    Remove ChangelogsController.h

--- Controllers/ChangelogsController.h
+++
@@ -1,9 +0,0 @@
-#import <WebKit/WebKit.h>
-#import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
-
-@interface ChangelogsController : UIViewController
-
-@property (nonatomic, strong) WKWebView *rebornChangelogsWebView;
-
-@end
