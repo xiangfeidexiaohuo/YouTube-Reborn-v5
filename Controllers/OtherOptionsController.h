afe68e00046d7073d2dfae0cb70c3d17e1dadcf0
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-17 14:32:58 -0600

    Update OtherOptionsController.h

--- Controllers/OtherOptionsController.h
+++ Controllers/OtherOptionsController.h
@@ -1,6 +1,5 @@
-#import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
+#import "UIKit/UIKit.h"
 
-@interface OtherOptionsController : UITableViewController
-
+@interface OtherOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView* tableView;
 @end
