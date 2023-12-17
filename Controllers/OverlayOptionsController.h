d2366941cd0f5987b5a4b85b31a96aa5433d461a
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-17 14:36:23 -0600

    Update OverlayOptionsController.h

--- Controllers/OverlayOptionsController.h
+++ Controllers/OverlayOptionsController.h
@@ -1,6 +1,5 @@
-#import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
+#import "UIKit/UIKit.h"
 
-@interface OverlayOptionsController : UITableViewController
-
+@interface OverlayOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView* tableView;
 @end
