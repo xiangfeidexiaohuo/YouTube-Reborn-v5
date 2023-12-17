fc819c30a1d2dce88613e34ebb359619bdea4248
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-17 14:32:19 -0600

    Update DownloadsAudioController.h

--- Controllers/DownloadsAudioController.h
+++ Controllers/DownloadsAudioController.h
@@ -1,7 +1,6 @@
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
 #import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
 
 @interface DownloadsAudioController : UIViewController <UITableViewDelegate, UITableViewDataSource, AVPlayerViewControllerDelegate>
 @property (nonatomic, strong) UITableView *tableView;
