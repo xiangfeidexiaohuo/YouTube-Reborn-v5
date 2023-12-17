3f6a4a01d072201d2bbf7f70c0428dca6351f396
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-17 14:42:28 -0600

    Update ShortsOptionsController.h

--- Controllers/ShortsOptionsController.h
+++ Controllers/ShortsOptionsController.h
@@ -1,6 +1,5 @@
-#import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
+#import "UIKit/UIKit.h"
 
-@interface ShortsOptionsController : UITableViewController
-
+@interface ShortsOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView* tableView;
 @end
