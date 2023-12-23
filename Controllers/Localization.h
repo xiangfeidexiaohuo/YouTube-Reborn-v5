a58aba446a0de2de7331550f9aca1cfd9f6caba1
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-23 13:43:32 -0600

    Update Localization.h

--- Controllers/Localization.h
+++ Controllers/Localization.h
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 
-extern NSBundle *YTRebornBundle();
+extern NSBundle *YouTubeRebornBundle();
 
 static inline NSString *LOC(NSString *key) {
-    NSBundle *tweakBundle = YTRebornBundle();
+    NSBundle *tweakBundle = YouTubeRebornBundle();
     return [tweakBundle localizedStringForKey:key value:nil table:nil];
 }
