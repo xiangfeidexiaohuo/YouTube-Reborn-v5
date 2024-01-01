91ee57b8c56bb56a061e49433b68dc20b0439079
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-01 00:52:03 -0600

    Update Localization.h

--- Controllers/Localization.h
+++ Controllers/Localization.h
@@ -6,3 +6,13 @@ static inline NSString *LOC(NSString *key) {
     NSBundle *tweakBundle = YouTubeRebornBundle();
     return [tweakBundle localizedStringForKey:key value:nil table:nil];
 }
+
+NSString *youtubeRebornLightSettingsPath;
+NSString *youtubeRebornDarkSettingsPath;
+if (YouTubeRebornBundle()) {
+    youtubeRebornLightSettingsPath = [[YouTubeRebornBundle() pathForResource:@"ytrebornbuttonwhite" ofType:@"png"]];
+    youtubeRebornDarkSettingsPath = [[YouTubeRebornBundle() pathForResource:@"ytrebornbuttonblack" ofType:@"png"]];
+} else {
+    youtubeRebornLightSettingsPath = ROOT_PATH_NS(@"/Library/Application Support/YouTubeReborn.bundle/ytrebornbuttonwhite.png");
+    youtubeRebornDarkSettingsPath = ROOT_PATH_NS(@"/Library/Application Support/YouTubeReborn.bundle/ytrebornbuttonblack.png");
+}
