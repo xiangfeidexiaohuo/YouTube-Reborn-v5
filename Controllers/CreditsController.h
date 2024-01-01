701c046fd79bfb5c719ee3b808cf3b1c9ab661c2
arichornlover <78001398+arichorn@users.noreply.github.com>
2023-12-31 23:08:00 -0600

    New Header for CreditsController
    New header that’s important for this new change to the Credits Controller otherwise I’ll get a building error.

--- Controllers/CreditsController.h
+++ Controllers/CreditsController.h
@@ -1,4 +1,5 @@
 #import "UIKit/UIKit.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CreditsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView* tableView;
