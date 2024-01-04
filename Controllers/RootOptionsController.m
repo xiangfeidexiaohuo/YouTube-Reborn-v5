f04bb8bc8b6ce8f995a0888ae1de51ce085427de
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-04 17:21:23 -0600

    Update RootOptionsController.m

--- Controllers/RootOptionsController.m
+++ Controllers/RootOptionsController.m
@@ -28,7 +28,7 @@
     [super viewDidLoad];
     [self coloursView];
 
-    self.title = @"Options";
+    self.title = @"YouTube Reborn";
     
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)];
     self.navigationItem.leftBarButtonItem = doneButton;
