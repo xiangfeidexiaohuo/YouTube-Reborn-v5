e3a0d2ec42c194f0c3e8dd81a2a24a77b507ec30
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-07 10:07:40 -0600

    Update RootOptionsController.h

--- Controllers/RootOptionsController.h
+++ Controllers/RootOptionsController.h
@@ -1,5 +1,5 @@
 #import "UIKit/UIKit.h"
 
-@interface RootOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface RootOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
 @property (nonatomic, strong) UITableView* tableView;
 @end
