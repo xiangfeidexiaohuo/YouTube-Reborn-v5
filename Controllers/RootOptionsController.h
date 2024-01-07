0b878d436724eac7ca1b4dfd0ee318c87958a7c7
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-07 10:12:56 -0600

    Update RootOptionsController.h

--- Controllers/RootOptionsController.h
+++ Controllers/RootOptionsController.h
@@ -2,4 +2,8 @@
 
 @interface RootOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
 @property (nonatomic, strong) UITableView* tableView;
+@property (nonatomic, strong) UISearchBar *searchBar;
+@property (nonatomic, strong) NSArray *allItems;
+@property (nonatomic, strong) NSArray *filteredItems;
+@property (nonatomic, assign) BOOL isSearching;
 @end
