ac49a92aeafacf1321a6b741f3d29cefb0053b4b
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-07 03:17:33 -0600

    Re-add DownloadsVideoController headers
    Y’know what, implement both?

--- Controllers/DownloadsVideoController.h
+++ Controllers/DownloadsVideoController.h
@@ -2,6 +2,7 @@
 #import <AVKit/AVKit.h>
 #import <UIKit/UIKit.h>
 
-@interface DownloadsVideoController : UIViewController <UITableViewDelegate, UITableViewDataSource, AVPlayerViewControllerDelegate>
+@interface DownloadsVideoController : UIViewController <UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, AVPlayerViewControllerDelegate>
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UICollectionView *collectionView;
 @end
