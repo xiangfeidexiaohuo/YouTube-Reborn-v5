3cd9b07a3e8bf3571d67269b6a9beace74f46bd0
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-07 10:12:30 -0600

    Update DownloadsVideoController.h

--- Controllers/DownloadsVideoController.h
+++ Controllers/DownloadsVideoController.h
@@ -4,4 +4,8 @@
 
 @interface DownloadsVideoController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, AVPlayerViewControllerDelegate>
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UISearchBar *searchBar;
+@property (nonatomic, strong) NSArray *allItems;
+@property (nonatomic, strong) NSArray *filteredItems;
+@property (nonatomic, assign) BOOL isSearching;
 @end
