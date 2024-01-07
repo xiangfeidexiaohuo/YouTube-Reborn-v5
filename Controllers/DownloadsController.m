4379d178acb7082298bc3e18473b28bfcc078a39
arichornlover <78001398+arichorn@users.noreply.github.com>
2024-01-07 11:43:43 -0600

    Update DownloadsController.m

--- Controllers/DownloadsController.m
+++ Controllers/DownloadsController.m
@@ -13,7 +13,6 @@
     [super viewDidLoad];
     [self configureUI];
 
-    NSBundle *tweakBundle = [NSBundle bundleForClass:[self class]];
     NSString *blackImageVideoPath = [tweakBundle pathForResource:@"ytrebornbuttonvideoblack" ofType:@"png"];
     NSString *whiteImageVideoPath = [tweakBundle pathForResource:@"ytrebornbuttonvideowhite" ofType:@"png"];
     NSString *blackImageAudioPath = [tweakBundle pathForResource:@"ytrebornbuttonaudioblack" ofType:@"png"];
