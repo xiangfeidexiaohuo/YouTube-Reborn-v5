92e3f6676253b3166fe5ca3f33f05de0e3e4fb9f
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-20 05:14:03 -0600

    Reimagined ReorderPivotBarController.m

--- Controllers/ReorderPivotBarController.m
+++ Controllers/ReorderPivotBarController.m
@@ -2,28 +2,26 @@
 #import "Localization.h"
 
 @interface ReorderPivotBarController ()
-- (void)coloursView;
 @end
 
 @implementation ReorderPivotBarController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self coloursView];
     
     self.title = LOC(@"REORDER_TABS");
     
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)];
-    UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithTitle:@"Save" style:UIBarButtonItemStylePlain target:self action:@selector(save)];
-    UIBarButtonItem *resetButton = [[UIBarButtonItem alloc] initWithTitle:@"Reset" style:UIBarButtonItemStylePlain target:self action:@selector(reset)];
+    UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithTitle:LOC(@"SAVE_TEXT") style:UIBarButtonItemStylePlain target:self action:@selector(save)];
+    UIBarButtonItem *resetButton = [[UIBarButtonItem alloc] initWithTitle:LOC(@"RESET_TEXT") style:UIBarButtonItemStylePlain target:self action:@selector(reset)];
     self.navigationItem.rightBarButtonItems = @[doneButton, saveButton, resetButton];
 
     UITableViewStyle style;
-        if (@available(iOS 13, *)) {
-            style = UITableViewStyleInsetGrouped;
-        } else {
-            style = UITableViewStyleGrouped;
-        }
+    if (@available(iOS 13, *)) {
+        style = UITableViewStyleInsetGrouped;
+    } else {
+        style = UITableViewStyleGrouped;
+    }
 
     self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:style];
     self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -40,11 +38,11 @@
 
     NSArray *savedTabOrder = [[NSUserDefaults standardUserDefaults] objectForKey:@"kTabOrder"];
     if (savedTabOrder != nil) {
-    self.tabOrder = [NSMutableArray arrayWithObjects:@"Home", @"Shorts", @"Create", @"Subscriptions", @"You", nil];
+        self.tabOrder = [NSMutableArray arrayWithObjects:@"Home", @"Shorts", @"Create", @"Subscriptions", @"You", nil];
 
-    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGesture:)];
-    [self.tableView addGestureRecognizer:longPressGesture];
-}
+        UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+        [self.tableView addGestureRecognizer:longPressGesture];
+    }
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -79,18 +77,18 @@
         }
     }
     
-if (indexPath.section == 1) {
-    if (indexPath.row == 0) {
-        cell.textLabel.text = @"Home";
-    } else if (indexPath.row == 1) { 
-        cell.textLabel.text = @"Shorts";
-    } else if (indexPath.row == 2) {
-        cell.textLabel.text = @"Create";
-    } else if (indexPath.row == 3) {
-        cell.textLabel.text = @"Subscriptions";
-    } else if (indexPath.row == 4) {
-        cell.textLabel.text = @"You";
-    }
+    if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+            cell.textLabel.text = @"Home";
+        } else if (indexPath.row == 1) { 
+            cell.textLabel.text = @"Shorts";
+        } else if (indexPath.row == 2) {
+            cell.textLabel.text = @"Create";
+        } else if (indexPath.row == 3) {
+            cell.textLabel.text = @"Subscriptions";
+        } else if (indexPath.row == 4) {
+            cell.textLabel.text = @"You";
+        }
 
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
     }
@@ -98,62 +96,48 @@ if (indexPath.section == 1) {
     return cell;
 }
 
-- (void)coloursView {
-    if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
-        self.view.backgroundColor = [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
-        [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor]}];
-        self.navigationController.navigationBar.barStyle = UIBarStyleDefault;
-    }
-    else {
-        self.view.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
-        [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
-        self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
-    }
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
 }
-- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
-    [super traitCollectionDidChange:previousTraitCollection];
-    [self coloursView];
-    [self.tableView reloadData];
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+    NSString *tabIdentifier = self.tabOrder[sourceIndexPath.row];
+    NSMutableArray *reorderedTabs = [NSMutableArray arrayWithArray:self.tabOrder];
+    if ([tabIdentifier isEqualToString:@"Home"]) {
+        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Home"];
+    } else if ([tabIdentifier isEqualToString:@"Shorts"]) {
+        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Shorts"];
+    } else if ([tabIdentifier isEqualToString:@"Create"]) {
+        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Create"];
+    } else if ([tabIdentifier isEqualToString:@"Subscriptions"]) {
+        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Subscriptions"];
+    } else if ([tabIdentifier isEqualToString:@"You"]) {
+        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"You"];
+    }
+    [self setTabOrder:reorderedTabs];
 }
+
 - (void)handleLongPress:(UILongPressGestureRecognizer *)gestureRecognizer {
     if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
         CGPoint location = [gestureRecognizer locationInView:self.tableView];
         NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
         if (indexPath && indexPath.section == 0) {
-            NSString *tabIdentifier = self.tabOrder[indexPath.row]; 
-            NSMutableArray *reorderedTabs = [NSMutableArray arrayWithArray:self.tabOrder];
-            if ([tabIdentifier isEqualToString:@"FEwhat_to_watch"]) {
-                [reorderedTabs replaceObjectAtIndex:indexPath.row withObject:@"Home"];
-            }
-                if ([tabIdentifier isEqualToString:@"FEshorts"]) {
-                [reorderedTabs replaceObjectAtIndex:indexPath.row withObject:@"Shorts"];
-                }
-                if ([tabIdentifier isEqualToString:@"FEuploads"]) {
-                [reorderedTabs replaceObjectAtIndex:indexPath.row withObject:@"Create"];
-                }
-                if ([tabIdentifier isEqualToString:@"FEsubscriptions"]) {
-                [reorderedTabs replaceObjectAtIndex:indexPath.row withObject:@"Subscriptions"];
-                }
-                if ([tabIdentifier isEqualToString:@"FElibrary"]) {
-                [reorderedTabs replaceObjectAtIndex:indexPath.row withObject:@"You"];
-                }
-            [self setTabOrder:reorderedTabs];
-            NSIndexPath *destinationIndexPath = [NSIndexPath indexPathForRow:reorderedTabs.count - 1 inSection:0];
-            [self.tableView beginUpdates];
-            [self.tableView moveRowAtIndexPath:indexPath toIndexPath:destinationIndexPath];
-            [self.tableView endUpdates];
+            [self.tableView setEditing:YES animated:YES];
         }
     }
 }
+
 - (void)reset {
     self.tabOrder = [NSMutableArray arrayWithObjects:@"Home", @"Shorts", @"Create", @"Subscriptions", @"You", nil];
     [self.tableView reloadData];
     [self save];
 }
+
 - (void)save {
     [[NSUserDefaults standardUserDefaults] setObject:self.tabOrder forKey:@"kTabOrder"];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
+
 - (void)done {   
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
