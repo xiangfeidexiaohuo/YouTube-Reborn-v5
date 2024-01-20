a82a62f4ac78ad5d8c9b5ab5c1d25fee2c1c6301
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-20 05:19:36 -0600

    New ReorderPivotBarController Header

--- Controllers/ReorderPivotBarController.h
+++ Controllers/ReorderPivotBarController.h
@@ -1,6 +1,7 @@
 #import "UIKit/UIKit.h"
 
 @interface ReorderPivotBarController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) YTIPivotBarController *pivotBarController;
 @property (nonatomic, strong) UITableView* tableView;
 @property (nonatomic, strong) NSMutableArray *tabOrder;
 @end
