007e7ea060a5327d00233a02a12cdc29977f5f94
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-23 01:27:37 -0600

    New OtherOptionsController Headers

--- Controllers/OtherOptionsController.h
+++ Controllers/OtherOptionsController.h
@@ -1,6 +1,7 @@
 #import "UIKit/UIKit.h"
 
-@interface OtherOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface OtherOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 @property (nonatomic, strong) UITableView* tableView;
 @property (nonatomic, copy) NSString *customAppVersion;
+@property (nonatomic, strong) UITextField *versionTextField;
 @end
