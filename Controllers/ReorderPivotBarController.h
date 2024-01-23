dcd4483a3120c405e52c6501491217db3b6b5b0a
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-22 22:22:04 -0600

    Update ReorderPivotBarController.h

--- Controllers/ReorderPivotBarController.h
+++ Controllers/ReorderPivotBarController.h
@@ -1,7 +1,7 @@
 #import "UIKit/UIKit.h"
 #import "../YouTubeHeader/YTIPivotBarRenderer.h"
 
-@interface ReorderPivotBarController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface ReorderPivotBarController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate>
 @property (nonatomic, strong) UITableView* tableView;
 @property (nonatomic, strong) NSMutableArray *tabOrder;
 @end
