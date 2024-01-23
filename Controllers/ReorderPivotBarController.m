1183ff168d66943f51a15d8917341bfeab6fde38
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-22 19:59:56 -0600

    Is this implementation smarter?

--- Controllers/ReorderPivotBarController.m
+++ Controllers/ReorderPivotBarController.m
@@ -39,7 +39,7 @@
 
     NSArray *savedTabOrder = [[NSUserDefaults standardUserDefaults] objectForKey:@"kTabOrder"];
     if (savedTabOrder != nil) {
-        self.tabOrder = [NSMutableArray arrayWithObjects:@"Home", @"Shorts", @"Create", @"Subscriptions", @"You", nil];
+        self.tabOrder = [NSMutableArray arrayWithArray:savedTabOrder];
 
         UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
         [self.tableView addGestureRecognizer:longPressGesture];
@@ -52,7 +52,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (section == 0) {
-        return 5;
+        return self.tabOrder.count;
     }
     return 0;
 }
@@ -79,17 +79,8 @@
     }
     
     if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            cell.textLabel.text = @"Home";
-        } else if (indexPath.row == 1) { 
-            cell.textLabel.text = @"Shorts";
-        } else if (indexPath.row == 2) {
-            cell.textLabel.text = @"Create";
-        } else if (indexPath.row == 3) {
-            cell.textLabel.text = @"Subscriptions";
-        } else if (indexPath.row == 4) {
-            cell.textLabel.text = @"You";
-        }
+        NSString *tabIdentifier = self.tabOrder[indexPath.row];
+        cell.textLabel.text = tabIdentifier;
 
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
     }
@@ -103,19 +94,7 @@
 
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
     NSString *tabIdentifier = self.tabOrder[sourceIndexPath.row];
-    NSMutableArray *reorderedTabs = [NSMutableArray arrayWithArray:self.tabOrder];
-    if ([tabIdentifier isEqualToString:@"Home"]) {
-        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Home"];
-    } else if ([tabIdentifier isEqualToString:@"Shorts"]) {
-        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Shorts"];
-    } else if ([tabIdentifier isEqualToString:@"Create"]) {
-        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Create"];
-    } else if ([tabIdentifier isEqualToString:@"Subscriptions"]) {
-        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"Subscriptions"];
-    } else if ([tabIdentifier isEqualToString:@"You"]) {
-        [reorderedTabs replaceObjectAtIndex:sourceIndexPath.row withObject:@"You"];
-    }
-    [self setTabOrder:reorderedTabs];
+    [self.tabOrder insertObject:tabIdentifier atIndex:destinationIndexPath.row];
 }
 
 - (void)handleLongPress:(UILongPressGestureRecognizer *)gestureRecognizer {
@@ -128,7 +107,7 @@
             NSInteger destinationIndex = [self.tableView numberOfRowsInSection:0] - 1;
             NSRange sourceRange = NSMakeRange(sourceIndex, 1);
             NSRange destinationRange = NSMakeRange(destinationIndex, 1);
-            [self.tabOrder replaceObjectsInRange:sourceRange withObjectsFromArray:[self.tabOrder subarrayWithRange:destinationRange]];
+            [self.tabOrder exchangeObjectsAtIndexes:sourceRange withObjectsAtIndexes:destinationRange];
         }
     }
 }
@@ -140,25 +119,7 @@
 }
 
 - (void)save {
-    NSMutableArray *orderedTabs = [NSMutableArray array];
-    for (NSIndexPath *indexPath in self.tableView.indexPathsForVisibleRows) {
-        if (indexPath.section == 0) {
-            NSString *tabIdentifier = @"";
-            if (indexPath.row == 0) {
-                tabIdentifier = @"Home";
-            } else if (indexPath.row == 1) {
-                tabIdentifier = @"Shorts";
-            } else if (indexPath.row == 2) {
-                tabIdentifier = @"Create";
-            } else if (indexPath.row == 3) {
-                tabIdentifier = @"Subscriptions";
-            } else if (indexPath.row == 4) {
-                tabIdentifier = @"You";
-            }
-            [orderedTabs addObject:tabIdentifier];
-        }
-    }
-    [[NSUserDefaults standardUserDefaults] setObject:orderedTabs forKey:@"kTabOrder"];
+    [[NSUserDefaults standardUserDefaults] setObject:self.tabOrder forKey:@"kTabOrder"];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
