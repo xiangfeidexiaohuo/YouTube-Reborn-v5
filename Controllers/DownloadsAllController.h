82a0b518286fd77ad7c2451a295fd2aac7e3cccc
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-25 21:41:48 -0600

    Create DownloadsAllController.h

---
+++ Controllers/DownloadsAllController.h
@@ -0,0 +1,10 @@
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
+#import <UIKit/UIKit.h>
+
+@interface DownloadsAllController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, AVPlayerViewControllerDelegate>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UISearchBar *searchBar;
+@property (nonatomic, strong) NSArray *allItems;
+@property (nonatomic, strong) NSArray *filteredItems;
+@property (nonatomic, assign) BOOL isSearching;
