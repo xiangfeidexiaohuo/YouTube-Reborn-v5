232a3c5e72de7c433f6409604adba693377aac1e
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-25 22:04:23 -0600

    Added DownloadsAllController.m

---
+++ Controllers/DownloadsAllController.m
@@ -0,0 +1,250 @@
+#import "DownloadsAllController.h"
+#import "Localization.h"
+#import <Photos/Photos.h>
+
+@interface DownloadsAllController ()
+{
+    NSString *documentsDirectory;
+    NSMutableArray *filePathsAudioArray;
+    NSMutableArray *filePathsAudioArtworkArray;
+    NSMutableArray *filePathsVideoArray;
+    NSMutableArray *filePathsVideoArtworkArray;
+}
+- (void)coloursView;
+- (void)setupAudioArrays;
+- (void)setupVideoArrays;
+@end
+
+@implementation DownloadsAllController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self coloursView];
+
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 44)];
+    self.searchBar.delegate = self;
+
+    UIBarButtonItem *searchButton = [[UIBarButtonItem alloc] initWithCustomView:self.searchBar];
+    self.navigationItem.rightBarButtonItem = searchButton;
+    self.filteredItems = [NSArray array];
+    self.isSearching = NO;
+
+    UITableViewStyle style;
+    if (@available(iOS 13, *)) {
+        style = UITableViewStyleInsetGrouped;
+    } else {
+        style = UITableViewStyleGrouped;
+    }
+
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:style];
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    [self setupAudioArrays];
+    [self setupVideoArrays];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.tableView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.tableView.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
+        [self.tableView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor],
+        [self.tableView.heightAnchor constraintEqualToAnchor:self.view.heightAnchor]
+    ]];
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    searchBar.text = @"";
+    self.filteredItems = [NSArray array];
+    self.isSearching = NO;
+    [self.tableView reloadData];
+    [searchBar resignFirstResponder];
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+    NSString *searchText = searchBar.text;
+
+    if (searchText.length > 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF BEGINSWITH[cd] %@", searchText];
+        self.filteredItems = [self.allItems filteredArrayUsingPredicate:predicate];
+        self.isSearching = YES;
+    } else {
+        self.filteredItems = [NSArray array];
+        self.isSearching = NO;
+    }
+    [self.tableView reloadData];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.isSearching) {
+        return self.filteredItems.count;
+    } else {
+        return self.allItems.count;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"VideoDownloadsTableViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+        cell.textLabel.adjustsFontSizeToFitWidth = YES;
+        cell.textLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+        cell.detailTextLabel.adjustsFontSizeToFitWidth = YES;
+        cell.detailTextLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+        if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
+            cell.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+            cell.textLabel.textColor = [UIColor blackColor];
+            cell.detailTextLabel.textColor = [UIColor blackColor];
+        }
+        else {
+            cell.backgroundColor = [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
+            cell.textLabel.textColor = [UIColor whiteColor];
+            cell.textLabel.shadowColor = [UIColor blackColor];
+            cell.textLabel.shadowOffset = CGSizeMake(1.0, 1.0);
+            cell.detailTextLabel.textColor = [UIColor whiteColor];
+        }
+    }
+    cell.textLabel.text = [filePathsVideoArray objectAtIndex:indexPath.row];
+    @try {
+        NSString *artworkFileName = filePathsVideoArtworkArray[indexPath.row];
+        cell.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@", [documentsDirectory stringByAppendingPathComponent:artworkFileName]]];
+    }
+    @catch (NSException *exception) {
+    }
+
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    NSString *currentFileName = filePathsVideoArray[indexPath.row];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:currentFileName];
+    
+    AVPlayerViewController *playerViewController = [AVPlayerViewController new];
+    playerViewController.player = [AVPlayer playerWithURL:[NSURL fileURLWithPath:filePath]];
+    playerViewController.allowsPictureInPicturePlayback = NO;
+    if (@available(iOS 14.2, *)) {
+        playerViewController.canStartPictureInPictureAutomaticallyFromInline = NO;
+    }
+    [playerViewController.player play];
+
+    [self presentViewController:playerViewController animated:YES completion:nil];
+}
+
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath {
+    NSString *currentVideoFileName = filePathsVideoArray[indexPath.row];
+    NSString *currentArtworkFileName = filePathsVideoArtworkArray[indexPath.row];
+
+    UIAlertController *alertMenu = [UIAlertController alertControllerWithTitle:LOC(@"OPTIONS_TEXT") message:nil preferredStyle:UIAlertControllerStyleAlert];
+
+    [alertMenu addAction:[UIAlertAction actionWithTitle:LOC(@"IMPORT_VIDEO") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+            NSURL *fileURL = [NSURL fileURLWithPath:[documentsDirectory stringByAppendingPathComponent:currentVideoFileName]];
+            [PHAssetChangeRequest creationRequestForAssetFromVideoAtFileURL:fileURL];
+        } completionHandler:^(BOOL success, NSError *error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (success) {
+                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:LOC(@"NOTICE_TEXT") message:LOC(@"SAVED_VIDEO") preferredStyle:UIAlertControllerStyleAlert];
+                                        
+                    [alert addAction:[UIAlertAction actionWithTitle:LOC(@"OKAY_TEXT") style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                    }]];
+
+                    [self presentViewController:alert animated:YES completion:nil];
+                } else{
+                    UIAlertController *alert = [UIAlertController alertControllerWithTitle:LOC(@"NOTICE_TEXT") message:LOC(@"SAVED_VIDEO_2") preferredStyle:UIAlertControllerStyleAlert];
+                                        
+                    [alert addAction:[UIAlertAction actionWithTitle:LOC(@"OKAY_TEXT") style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                    }]];
+
+                    [self presentViewController:alert animated:YES completion:nil];
+                }
+            });
+        }];
+    }]];
+
+    [alertMenu addAction:[UIAlertAction actionWithTitle:LOC(@"DELETE_VIDEO") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [[NSFileManager defaultManager] removeItemAtPath:[documentsDirectory stringByAppendingPathComponent:currentVideoFileName] error:nil];
+        [[NSFileManager defaultManager] removeItemAtPath:[documentsDirectory stringByAppendingPathComponent:currentArtworkFileName] error:nil];
+
+        UIAlertController *alertDeleted = [UIAlertController alertControllerWithTitle:LOC(@"NOTICE_TEXT") message:LOC(@"VIDEO_DELETED") preferredStyle:UIAlertControllerStyleAlert];
+
+        [alertDeleted addAction:[UIAlertAction actionWithTitle:LOC(@"OKAY_TEXT") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            [filePathsVideoArray removeAllObjects];
+            [filePathsVideoArtworkArray removeAllObjects];
+            [self setupVideoArrays];
+            [self.tableView reloadData];
+        }]];
+
+        [self presentViewController:alertDeleted animated:YES completion:nil];
+    }]];
+
+    [alertMenu addAction:[UIAlertAction actionWithTitle:LOC(@"CANCEL_TEXT") style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+    }]];
+
+    [self presentViewController:alertMenu animated:YES completion:nil];
+}
+
+- (void)setupVideoArrays {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    documentsDirectory = [paths objectAtIndex:0];
+
+    NSArray *filePathsList = [[NSFileManager defaultManager] subpathsOfDirectoryAtPath:documentsDirectory error:nil];
+    filePathsVideoArray = [[NSMutableArray alloc] init];
+    filePathsVideoArtworkArray = [[NSMutableArray alloc] init];
+    for (id object in filePathsList) {
+        if ([[object pathExtension] isEqualToString:@"mp4"]){
+            [filePathsVideoArray addObject:object];
+            NSString *cut = [object substringToIndex:[object length]-4];
+            NSString *jpg = [NSString stringWithFormat:@"%@.jpg", cut];
+            [filePathsVideoArtworkArray addObject:jpg];
+        }
+    }
+    self.allItems = [NSArray arrayWithArray:filePathsVideoArray];
+}
+
+- (void)setupAudioArrays {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    documentsDirectory = [paths objectAtIndex:0];
+
+    NSArray *filePathsList = [[NSFileManager defaultManager] subpathsOfDirectoryAtPath:documentsDirectory error:nil];
+    filePathsAudioArray = [[NSMutableArray alloc] init];
+    filePathsAudioArtworkArray = [[NSMutableArray alloc] init];
+    for (id object in filePathsList) {
+        if ([[object pathExtension] isEqualToString:@"m4a"] || [[object pathExtension] isEqualToString:@"mp3"]){
+            [filePathsAudioArray addObject:object];
+            NSString *cut = [object substringToIndex:[object length]-4];
+            NSString *jpg = [NSString stringWithFormat:@"%@.jpg", cut];
+            [filePathsAudioArtworkArray addObject:jpg];
+        }
+    }
+    self.allItems = [NSArray arrayWithArray:filePathsAudioArray];
+}
+
+- (void)coloursView {
+    if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
+        self.view.backgroundColor = [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
+    }
+    else {
+        self.view.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+    }
+}
+
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+    [super traitCollectionDidChange:previousTraitCollection];
+    [self coloursView];
+    [self.tableView reloadData];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+@end
