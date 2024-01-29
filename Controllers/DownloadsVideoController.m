4f71d2355e2b407429aefc31ec0f516eb8bdc76d
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-29 16:05:04 -0600

    Update DownloadsVideoController.m

--- Controllers/DownloadsVideoController.m
+++ Controllers/DownloadsVideoController.m
@@ -111,7 +111,8 @@
             cell.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
             cell.textLabel.textColor = [UIColor blackColor];
             cell.detailTextLabel.textColor = [UIColor blackColor];
-        } else {
+        }
+        else {
             cell.backgroundColor = [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
             cell.textLabel.textColor = [UIColor whiteColor];
             cell.textLabel.shadowColor = [UIColor blackColor];
@@ -120,21 +121,13 @@
         }
     }
     cell.textLabel.text = [filePathsVideoArray objectAtIndex:indexPath.row];
-    UIImage *previewImage = nil;
     @try {
         NSString *artworkFileName = filePathsVideoArtworkArray[indexPath.row];
-        previewImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@", [documentsDirectory stringByAppendingPathComponent:artworkFileName]]];
-        if (CGSizeEqualToSize(previewImage.size, CGSizeZero)) {
-            previewImage = [UIImage imageNamed:@"placeholder_image"];
-        }
+        cell.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@", [documentsDirectory stringByAppendingPathComponent:artworkFileName]]];
     }
     @catch (NSException *exception) {
     }
-    UIImageView *previewImageView = [[UIImageView alloc] initWithImage:previewImage];
-    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    cell.imageView.clipsToBounds = YES;
-    cell.imageView.image = nil;
-    cell.imageView.image = previewImageView;
+
     return cell;
 }
 
@@ -221,19 +214,6 @@
             NSString *cut = [object substringToIndex:[object length]-4];
             NSString *jpg = [NSString stringWithFormat:@"%@.jpg", cut];
             [filePathsVideoArtworkArray addObject:jpg];
-        }
-    }
-    UIFont *font = [UIFont systemFontOfSize:17.0];
-    for (NSInteger i = 0; i < filePathsVideoArray.count; i++) {
-        NSString *filePath = filePathsVideoArray[i];
-        NSString *text = filePath.lastPathComponent;
-        CGSize requiredSize = [text sizeWithAttributes:@{NSFontAttributeName: font}];
-      
-        CGFloat availableWidth = self.tableView.frame.size.width - 40.0;
-        if (requiredSize.width > availableWidth) {
-            NSInteger charactersToRemove = (requiredSize.width - availableWidth) / requiredSize.width * text.length;
-            NSString *resizedText = [text stringByReplacingCharactersInRange:NSMakeRange(text.length - charactersToRemove, charactersToRemove) withString:@"..."];
-            filePathsVideoArray[i] = [resizedText copy];
         }
     }
     self.allItems = [NSArray arrayWithArray:filePathsVideoArray];
