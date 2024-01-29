b7190763ceb4a68eade58e7cdd54a22bd13669e5
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-28 18:09:37 -0600

    Update ReorderPivotBarController.m

--- Controllers/ReorderPivotBarController.m
+++ Controllers/ReorderPivotBarController.m
@@ -93,7 +93,7 @@
 }
 
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
-    NSInteger sourceIndex = sourceIndexPath.row;
+    NSInteger sourceIndex = indexPath.row;
     NSInteger destinationIndex = destinationIndexPath.row;
 
     NSString *tabIdentifier = self.tabOrder[sourceIndex];
@@ -107,7 +107,7 @@
         NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
         if (indexPath && indexPath.section == 0) {
             [self.tableView setEditing:YES animated:YES];
-            NSInteger sourceIndex = sourceIndexPath.row;
+            NSInteger sourceIndex = indexPath.row;
             NSInteger destinationIndex = [self.tableView numberOfRowsInSection:0] - 1;
             NSIndexPath *destinationIndexPath = [NSIndexPath indexPathForRow:destinationIndex inSection:0];
             [self.tableView moveRowAtIndexPath:indexPath toIndexPath:destinationIndexPath];
