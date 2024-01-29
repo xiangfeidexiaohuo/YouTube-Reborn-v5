d26cda1b3a5f67a23d04bc6934e92ef18b7c6da0
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-28 18:32:08 -0600

    Update CreditsController.h

--- Controllers/CreditsController.h
+++ Controllers/CreditsController.h
@@ -1,5 +1,4 @@
 #import <UIKit/UIKit.h>
-#import "SDWebImage/Core/UIImageView+WebCache.h"
 
 @interface CreditsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView* tableView;
