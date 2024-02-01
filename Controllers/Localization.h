79e1afbbe20c272e6de7ff21344cdde2d105085d
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-31 20:47:24 -0600

    Add TabBarOPIconPath to Localization.h
    TabBarOPIconPath is added so it can be used in settings!

--- Controllers/Localization.h
+++ Controllers/Localization.h
@@ -1,8 +1,10 @@
 #import <Foundation/Foundation.h>
+static NSString *TabBarOPIconPath;
 
 extern NSBundle *YouTubeRebornBundle();
 
 static inline NSString *LOC(NSString *key) {
     NSBundle *tweakBundle = YouTubeRebornBundle();
+    TabBarOPIconPath = [tweakBundle pathForResource:@"ytrebornbuttonblack" ofType:@"png"];
     return [tweakBundle localizedStringForKey:key value:nil table:nil];
 }
