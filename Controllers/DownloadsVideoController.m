dd2b7a28ba4102b3b52e263cf5b31861a19e872e
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-02 18:42:30 -0600

    Update DownloadsVideoController.m

--- Controllers/DownloadsVideoController.m
+++ Controllers/DownloadsVideoController.m
@@ -70,7 +70,8 @@
     NSString *searchText = searchBar.text;
 
     if (searchText.length > 0) {
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF CONTAINS[cd] %@", searchText];
+        NSString *cleanSearchText = [searchText stringByReplacingOccurrencesOfString:@" " withString:@""];
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF CONTAINS[cd] %@", cleanSearchText];
         self.filteredItems = [self.allItems filteredArrayUsingPredicate:predicate];
         self.isSearching = YES;
     } else {
@@ -103,9 +104,11 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
         cell.textLabel.adjustsFontSizeToFitWidth = YES;
-        cell.textLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+        cell.textLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        cell.textLabel.marqueeScrollEnabled = YES;
         cell.detailTextLabel.adjustsFontSizeToFitWidth = YES;
-        cell.detailTextLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+        cell.detailTextLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        cell.detailTextLabel.marqueeScrollEnabled = YES;
         cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
         if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
             cell.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
