b292aad47d4418b75e6f88e9d3a2392bd77989bc
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-06 17:19:03 -0600

    Update PictureInPictureOptionsController.m

--- Controllers/PictureInPictureOptionsController.m
+++ Controllers/PictureInPictureOptionsController.m
@@ -14,7 +14,7 @@
     self.title = LOC(@"PICTURE_IN_PICTURE_OPTIONS");
 
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)];
-    self.navigationItem.rightBarButtonItem = doneButton;
+    self.navigationItem.leftBarButtonItem = doneButton;
 
     UITableViewStyle style;
         if (@available(iOS 13, *)) {
