1c10285e77cdae6e9ee5351dbc20a9b408385f37
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-07 23:10:46 -0600

    Update DownloadsAllController.m

--- Controllers/DownloadsAllController.m
+++ Controllers/DownloadsAllController.m
@@ -179,7 +179,9 @@
 
     NSString *currentFileName = filePathsAllArray[indexPath.row];
     NSString *filePath = [documentsDirectory stringByAppendingPathComponent:currentFileName];
-    
+
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+
     AVPlayerViewController *playerViewController = [AVPlayerViewController new];
     playerViewController.player = [AVPlayer playerWithURL:[NSURL fileURLWithPath:filePath]];
     playerViewController.allowsPictureInPicturePlayback = NO;
