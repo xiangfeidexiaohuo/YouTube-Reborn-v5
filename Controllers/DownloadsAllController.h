e23e700a03fd35c4e01f126b9ae65cd014f2fe1f
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-14 16:45:04 -0600

    Update DownloadsAllController.h

--- Controllers/DownloadsAllController.h
+++ Controllers/DownloadsAllController.h
@@ -5,6 +5,7 @@
 
 @interface DownloadsAllController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UIScrollViewDelegate, AVPlayerViewControllerDelegate>
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UISearchBar *searchBar;
 @property (nonatomic, strong) NSArray *allItems;
 @property (nonatomic, strong) NSArray *filteredItems;
