11b578c96fa949967330b16635f535270df5bb62
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-21 18:04:27 -0600

    Update and rename to ColourOptionsController4.h
    ColourOptionsController3 is gonna be something related to Themes and Tint now, you will be able to replace the systemBlueColor in this one. Which means you’ll have another way to customize the app.

--- Controllers/ColourOptionsController3.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface ColourOptionsController3 : UIColorPickerViewController <UIColorPickerViewControllerDelegate>
-
-@end
---
+++ Controllers/ColourOptionsController4.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface ColourOptionsController4 : UIColorPickerViewController <UIColorPickerViewControllerDelegate>
+
+@end
