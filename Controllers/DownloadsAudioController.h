b681720db863dbf8ba0884592eceb1e4e3640092
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-02-28 20:02:08 -0600

    Update DownloadsAudioController.h

--- Controllers/DownloadsAudioController.h
+++ Controllers/DownloadsAudioController.h
@@ -3,7 +3,7 @@
 #import <AVKit/AVKit.h>
 #import <UIKit/UIKit.h>
 
-@interface DownloadsAudioController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UIScrollViewDelegate, AVPlayerViewControllerDelegate>
+@interface DownloadsAudioController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UIScrollViewDelegate, UIDocumentPickerDelegate, AVPlayerViewControllerDelegate>
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UISearchBar *searchBar;
