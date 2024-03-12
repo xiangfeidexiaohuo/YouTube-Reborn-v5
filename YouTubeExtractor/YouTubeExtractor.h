627fb8e628b61a320f579a9a0caa9f87f4539c6a
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-03-11 22:40:05 -0500

    Update YouTubeExtractor.h

--- YouTubeExtractor/YouTubeExtractor.h
+++ YouTubeExtractor/YouTubeExtractor.h
@@ -1,32 +1,5 @@
 #import <Foundation/Foundation.h>
 
-@interface YTIVideoDetails : NSObject
-@property (nonatomic, copy, readwrite) NSString *title;
-@property (nonatomic, copy, readwrite) NSString *author;
-@property (nonatomic, strong, readwrite) YTIThumbnailDetails *thumbnail;
-@end
-
-@interface YTIPlayerResponse : NSObject
-@property (nonatomic, assign, readonly) YTIStreamingData *streamingData;
-@property (nonatomic, assign, readonly) YTIVideoDetails *videoDetails;
-@end
-
-@interface YTPlayerResponse : NSObject
-@property (nonatomic, assign, readonly) YTIPlayerResponse *playerData;
-@end
-
-@interface YTPlayerViewController : UIViewController
-@property (nonatomic, assign, readonly) YTPlayerResponse *playerResponse;
-@property (readonly, nonatomic) NSString *contentVideoID;
-@property (nonatomic, assign, readonly) CGFloat currentVideoTotalMediaTime;
-@end
-
-@interface YTPlayerView : UIView
-@property (nonatomic, strong) FFMpegDownloader *ffmpeg;
-@property (nonatomic, assign, readwrite) UIViewController *viewDelegate;
-- (void)prepareForDownloading:(UILongPressGestureRecognizer *)sender;
-@end
-
 @interface YouTubeExtractor : NSObject
 + (NSDictionary *)youtubePlayerRequest :(NSString *)client :(NSString *)videoID;
 @end
