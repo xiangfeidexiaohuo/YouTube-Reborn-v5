468201dbc4231daac534f385305048fae7578d23
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-03-30 15:36:04 -0500

    Update RebornSettingsController.h

--- Controllers/RebornSettingsController.h
+++ Controllers/RebornSettingsController.h
@@ -3,3 +3,19 @@
 @interface RebornSettingsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView* tableView;
 @end
+
+// snackbar (RebornSettingsController)
+@interface YTHUDMessage : NSObject
++ (id)messageWithText:(id)text;
+- (void)setAction:(id)action;
+@end
+
+@interface GOOHUDMessageAction : NSObject
+- (void)setTitle:(NSString *)title;
+- (void)setHandler:(void (^)(id))handler;
+@end
+
+@interface GOOHUDManagerInternal : NSObject
+- (void)showMessageMainThread:(id)message;
++ (id)sharedInstance;
+@end
