e7b92294686b8976d077b5753957a435e9bdfbd3
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-03-31 00:16:37 -0500

    Update RootOptionsController.m

--- Controllers/RootOptionsController.m
+++ Controllers/RootOptionsController.m
@@ -37,8 +37,8 @@
     
     if ([currentVersion compare:requiredVersion options:NSNumericSearch] == NSOrderedAscending) {
         dispatch_async(dispatch_get_main_queue(), ^{
-        YTHUDMessage *message = [%c(YTHUDMessage) messageWithText:text];
-        GOOHUDManagerInternal *manager = [%c(GOOHUDManagerInternal) sharedInstance];
+        YTHUDMessage *message = [YTHUDMessage messageWithText:[NSString stringWithFormat:@"You are using the Client version %@. Please use at least version %@ or higher.", currentVersion, requiredVersion]];
+        GOOHUDManagerInternal *manager = [GOOHUDManagerInternal sharedInstance];
         [manager showMessageMainThread:message];
         });
         [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
