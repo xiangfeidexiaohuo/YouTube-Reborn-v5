fd8c6591e01d68df0064c2396721d3ef44e68d4d
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-03-30 20:47:05 -0500

    Update Tweak.h

--- Tweak.h
+++ Tweak.h
@@ -216,3 +216,19 @@
 
 @interface SponsorBlockViewController : UIViewController
 @end
+
+// snackbar
+@interface YTHUDMessage : NSObject
++ (id)messageWithText:(id)text;
+- (void)setAction:(id)action;
+@end
+
+@interface GOOHUDMessageAction : NSObject
+- (void)setTitle:(NSString *)title;
+- (void)setHandler:(void (^)(id))handler;
+@end
+
+@interface GOOHUDManagerInternal : NSObject
+- (void)showMessageMainThread:(id)message;
++ (id)sharedInstance;
+@end
