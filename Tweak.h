312858884dd4e068bdd45570a01ad5a92c8299fa
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-05-17 16:37:02 -0500

    Update Tweak.h

--- Tweak.h
+++ Tweak.h
@@ -17,6 +17,9 @@
 #import "YouTubeHeader/ELMCellNode.h"
 #import "YouTubeHeader/ELMContainerNode.h"
 #import "YouTubeHeader/ELMNodeController.h"
+#import "YouTubeHeader/YTIFormattedString.h"
+#import "YouTubeHeader/GPBMessage.h"
+#import "YouTubeHeader/YTIStringRun.h"
 #import "YouTubeHeader/QTMIcon.h"
 #import "YouTubeHeader/YTColor.h"
 #import "YouTubeHeader/YTColorPalette.h"
@@ -138,6 +141,43 @@
 
 @interface YTWatchController : NSObject
 - (void)showFullScreen;
+@end
+
+@interface YTIIconThumbnailRenderer : GPBMessage
+    @property (nonatomic, strong) YTIIcon *icon;
+    - (bool)hasIcon;
+@end
+@interface YTICompactListItemThumbnailSupportedRenderers : GPBMessage
+    @property (nonatomic, strong) YTIIconThumbnailRenderer *iconThumbnailRenderer;
+    - (bool)hasIconThumbnailRenderer;
+@end
+@interface YTICompactListItemRenderer : GPBMessage
+    @property (nonatomic, strong) YTICompactListItemThumbnailSupportedRenderers *thumbnail;
+    @property (nonatomic, strong) YTIFormattedString *title;
+    - (bool)hasThumbnail;
+    - (bool)hasTitle;
+@end
+@interface YTIIcon (uYouEnhanced)
+    - (bool)hasIconType;
+@end
+@interface YTICompactLinkRenderer : GPBMessage
+    @property (nonatomic, strong) YTIIcon *icon;
+    @property (nonatomic, strong) YTIFormattedString *title;
+    @property (nonatomic, strong) YTICompactListItemThumbnailSupportedRenderers *thumbnail;
+    - (bool)hasIcon;
+    - (bool)hasThumbnail;
+@end
+@interface YTIItemSectionSupportedRenderers (uYouEnhanced)
+    @property(readonly, nonatomic) YTICompactLinkRenderer *compactLinkRenderer;
+    @property(readonly, nonatomic) YTICompactListItemRenderer *compactListItemRenderer;
+    - (bool)hasCompactLinkRenderer;
+    - (bool)hasCompactListItemRenderer;
+@end
+@interface YTAppCollectionViewController : YTInnerTubeCollectionViewController
+- (void)uYouEnhancedFakePremiumModel:(YTISectionListRenderer *)model;
+@end
+@interface YTInnerTubeCollectionViewController (uYouEnhanced)
+    @property(readonly, nonatomic) YTISectionListRenderer *model;
 @end
 
 @interface YTSingleVideoTime : NSObject
